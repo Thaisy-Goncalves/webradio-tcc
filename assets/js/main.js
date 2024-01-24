ff0b026b096f5fef9f80e4b3b52ba1076c2c4e8e
Thaisy-Gomes <thaisygoncalvesjg@gmail.com>
2024-01-24 00:23:26 -0300

    feat: é um pequeno avanço, agr esse play toca

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,4 +33,47 @@ function colorLink() {
     }
 }
 
-linkColor.forEach(l => l.addEventListener('click', colorLink))
+linkColor.forEach(l => l.addEventListener('click', colorLink));
+
+
+var player = document.getElementById('player');
+let progress = document.getElementById('progress');
+let playbtn = document.getElementById('playbtn');
+
+var playpause = function () {
+    if (player.paused) {
+        player.play();
+    } else {
+        player.pause();
+    }
+}
+
+playbtn.addEventListener('click', playpause);
+
+player.onplay = function () {
+    playbtn.classList.remove('bx-play');
+    playbtn.classList.add('bx-pause');
+}
+
+player.onpause = function () {
+    playbtn.classList.add('bx-play');
+    playbtn.classList.remove('bx-pause');
+}
+
+player.ontimeupdate = function () {
+    let ct = player.currentTime;
+    current.innerHtml = timeFormat(ct);
+    
+    let duration = player.duration;
+    prog = Math.floor((ct*100)/duration);
+    progress.style.setProperty("--progress", prog + '%');
+}      
+
+function timeFormat(ct) {
+    minutes = Math.floor(ct / 60);
+    seconds = Math.floor(ct % 60);
+    if (seconds < 10) {
+        seconds = "0"+seconds;
+    }
+    return minutes + ':' + seconds;
+}
